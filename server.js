02128d868f9a3d1a93ad5958482fb84b5451b7d7
Alexandre <alexandre.krispin@vinx.co.jp>
2017-02-17 17:59:38 +0900

    Modularize app

--- server.js
+++ server.js
@@ -3,22 +3,7 @@ import Hoek from 'hoek';
 import Vision from 'vision';
 import HapiAuthJWT from 'hapi-auth-jwt2';
 import HapiReactViews from 'hapi-react-views';
-import Nano from 'nano';
-
-const dbUrl = 'http://localhost:5984';
-
-Nano(dbUrl).auth('admin', 'admin', (err, body, headers) => {
-  if (err) return console.log(err);
-  if (headers && headers['set-cookie']) {
-      let auth;
-      auth = headers['set-cookie'];
-  }
-  console.log('Authenticated');
-});
-
-const posts = Nano(dbUrl).db.use('posts');
-const users = Nano(dbUrl).db.use('users');
-const viewPostsUrl = '_view/all';
+import routes from './lib/routes';
 
 const server = new Hapi.Server({
     debug: {
@@ -57,7 +42,7 @@ server.register(HapiAuthJWT, err => {
     server.auth.default('jwt');
 })
 
-server.register(Vision, (err) => {
+server.register(Vision, err => {
   Hoek.assert(!err, err);
 
   server.views({
@@ -70,47 +55,7 @@ server.register(Vision, (err) => {
   });
 });
 
-server.route([{
-    method: 'GET',
-    path: '/posts',
-    config: { auth: false },
-    handler: getPosts
-},
-{
-    method: 'GET',
-    path: '/posts/{id}',
-    config: { auth: false },
-    handler: getPost
-},
-{
-    method: 'POST',
-    path: '/auth',
-    config: { auth: false },
-    handler: verifyCredentials
-}])
-
-function getPosts(request, reply) {
-    posts.view('posts', 'all', (err, body) => {
-        if (err) throw err;
-        reply({data: body.rows});
-    });
-}
-
-function getPost(request, reply) {
-    console.log(request.params.id);
-    posts.get(request.params.id, (err, body) => {
-        if (err) throw err;
-        reply({data: body})
-            .header('Authorization', request.headers.authorization);
-    })
-}
-
-function verifyCredentials(request, reply) {
-    const { username, password } = request.payload;
-    /*users.get('admin', { revs_info: true }, function(err, body) {
-        if (!err) console.log(body);
-    });*/
-}
+server.route(routes)
 
 server.start((err) => {
   if (err) console.log(err);
